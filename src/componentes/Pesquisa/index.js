84fd51e9f9fb9584147232582843906ce0a85304
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2024-03-24 17:25:53 -0300

    Requisições HTTP com Axios

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,7 @@
 import Input from '../Input'
 import styled from 'styled-components'
-import { useState } from 'react'
-import { livros } from './dadosPesquisa'
+import { getLivros } from '../../servicos/livros'
+import { useEffect, useState } from 'react'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -44,9 +44,18 @@ const Resultado = styled.div`
         border: 1px solid white;
     }
 `
-
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [livros, setLivros] = useState([])
+
+    useEffect(() => {
+        fetchLivros()
+    }, [])
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros()
+        setLivros(livrosDaAPI)
+    }
 
     return (
         <PesquisaContainer>
